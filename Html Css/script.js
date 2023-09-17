6d14e70835a24939411329390a0886a6466d24b0
Ossama kharbaq <ossamakharbaq4@gmail.com>
2023-09-17 17:47:57 +0100

    the job browsing template has been finished

---
+++ Html Css/script.js
@@ -0,0 +1,12 @@
+function toggleNav(){
+    let nav = document.querySelector('.navigation');
+    nav.classList.toggle('close');
+}
+function toggleFilterOption(element){
+    let parentElement = element.parentElement;
+    parentElement.classList.toggle('close');
+}
+function toggleFilter(){
+    let element = document.querySelector('.results-displayer > div > div');
+    element.classList.toggle('open');
+}
