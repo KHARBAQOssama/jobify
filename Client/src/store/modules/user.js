bb20772470df9eaed2a1e286812db234fe019d88
Ossama kharbaq <ossamakharbaq4@gmail.com>
2024-01-04 16:47:07 +0100

    Updated business logic in store/modules/user.js

--- Client/src/store/modules/user.js
+++ Client/src/store/modules/user.js
@@ -1,23 +1,28 @@
 import api from "../../config/api";
 
 let state = {
-  user: localStorage.getItem("user")
-    ? JSON.parse(localStorage.getItem("user"))
-    : {
-        email: null,
-        password: null,
-        password_confirmation: null,
-      },
-  profile: {},
+  token: localStorage.getItem("token") || null,
+  user: JSON.parse(localStorage.getItem("user")) || null,
+  isAuthenticated: localStorage.getItem("token") !== null,
   message: {},
 };
+
 let mutations = {
   setUser(state, user) {
     state.user = user;
     localStorage.setItem("user", JSON.stringify(user));
   },
   setToken(state, token) {
+    state.token = token;
+    state.isAuthenticated = true;
     localStorage.setItem("token", JSON.stringify(token));
+  },
+  clearAuth(state) {
+    state.token = null;
+    state.user = null;
+    state.isAuthenticated = false;
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
   },
   setMessage(state, message) {
     state.message = message;
@@ -29,21 +34,69 @@ let mutations = {
     state.profile = profile;
   },
 };
+
 let actions = {
-  async getAuthUser({ commit }) {
+  async login({ state, dispatch, commit },credentials) {
+    console.log(credentials);
     try {
-      const response = await api.get("/auth/me");
+      const response = await api.post("/auth/login", credentials);
+      if (response.status === 200) {
+        commit("setToken", response.data.token);
+        dispatch("getAuthUser");
+      }
+      return response;
+    } catch (error) {
+      commit("setMessage", {
+        type: "danger",
+        content: error.response.data.message.split(".")[0],
+      });
+      return error.response;
+    }
+  },
+  removeLocalStorageItems() {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+  },
+  async logout({ commit, dispatch }) {
+    try {
+      const response = await api.post("/auth/logout");
+      if (response.status === 200) {
+        dispatch("removeLocalStorageItems");
+        commit("setUser", {
+          email: null,
+          password: null,
+          password_confirmation: null,
+        });
+      }
+      return response;
+    } catch (error) {
+      commit("setMessage", {
+        type: "danger",
+        content: error.response.data.message.split(".")[0],
+      });
+      return error.response;
+    }
+  },
+  async getAuthUser({ commit, dispatch }) {
+    try {
+      const response = await api.post("/auth/me");
       if (response.status === 200) {
         commit("setUser", response.data.user);
       }
       return response;
     } catch (error) {
+      dispatch("removeLocalStorageItems");
+      commit("setUser", {
+        email: null,
+        password: null,
+        password_confirmation: null,
+      });
       return error.response;
     }
   },
-  async register({ commit, state }) {
+  async register({ commit, state }, credentials) {
     try {
-      const response = await api.post("/auth/register", state.user);
+      const response = await api.post("/auth/register", credentials);
       if (response.status === 200) {
         commit("setUser", response.data.user);
         commit("setToken", response.data.token);
@@ -57,36 +110,21 @@ let actions = {
       console.log(error.response);
       commit("setMessage", {
         type: "danger",
-        content: error.response.data.message.split('.')[0]
+        content: error.response.data.message.split(".")[0],
       });
       return error.response;
     }
   },
-  async initializeRole({ commit, state }) {
+  async initializeRole({ commit, state }, role) {
     try {
       const response = await api.put("/auth/initialize-role", {
-        role: state.user.role_id,
+        role
       });
       console.log(response);
       if (response.status === 200) {
         commit("setUser", response.data.user);
         commit("setMessage", {});
-        commit(
-          "setProfile",
-          state.user.role_id == 2
-            ? {
-                first_name: null,
-                last_name: null,
-                birthday: null,
-                occupation: null,
-              }
-            : {
-                name: null,
-                foundation_date: null,
-                industry_id: null,
-                new_industry: null,
-              }
-        );
+        console.log(state.user);
       }
       return response;
     } catch (error) {
@@ -97,11 +135,11 @@ let actions = {
       return error.response;
     }
   },
-  async completeProfile({ commit, state }) {
+  async completeProfile({ commit, state }, credentials) {
     console.log(state.profile);
     try {
-      if (state.profile.new_industry) state.profile.industry_id = null;
-      const response = await api.post("/auth/complete-profile", state.profile);
+      if (credentials.new_industry) credentials.industry_id = null;
+      const response = await api.post("/auth/complete-profile", credentials);
       console.log(response);
       if (response.status === 200) {
         commit("setUser", response.data.user);
