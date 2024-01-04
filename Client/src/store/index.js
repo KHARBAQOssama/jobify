d964fb93a452acc17729336e5021fe3967299bff
Ossama kharbaq <ossamakharbaq4@gmail.com>
2024-01-04 16:47:02 +0100

    Changed business logic in store/index.js

--- Client/src/store/index.js
+++ Client/src/store/index.js
@@ -1,8 +1,11 @@
 import { createStore } from "vuex";
 import userStore from './modules/user'
+import companyStore from './modules/company'
 
 let modules = {
   userStore,
+  companyStore,
+  
 };
 
 export default createStore({
